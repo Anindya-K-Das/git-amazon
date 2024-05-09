f276d13e48a22cec6d277974b0a86dbd8de79573
Das <anindyadas2004@gmail.com>
2024-05-08 22:42:39 -0400

    OOP cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/cartOP.js';
+
 
 renderPaymentSummary();
 renderOrderSummary();
